f7914c265a1365b7df7aec0f71c50b7922dcc2e5
LEEJEEWON <leejiwon6315@naver.com>
2020-08-13 19:50:32 +0900

    Day10 challenge code

---
+++ 바닐라JS 2주 완성반/Day10.js
@@ -0,0 +1,58 @@
+const calcHistDiv = document.getElementById(`jsCalcHistory`);
+const panelForm = document.getElementById(`jsPanel`);
+const panel = panelForm.querySelector(`h3`);
+const regExp = document.getElementsByClassName(`regExpButton`);
+
+let inputFormula = ``;
+let numCheck = true;
+//연속된(잘못된) 계산 부호를 체크해주는 변수. 가장 먼저 입력했던 부호만 계산에 입력하기 위해 생성
+
+function add(char) {
+  numCheck = true;
+
+  if (inputFormula === ``) {
+    panel.innerText = char;
+    inputFormula += char;
+  } else {
+    panel.innerText += char;
+    inputFormula += char;
+  }
+}
+
+function formula(char) {
+  if (inputFormula === ``) {
+    if (char === "-") {
+      panel.innerText = `-`;
+      inputFormula += char;
+    }
+  } else {
+    if (numCheck === false) {
+    } else {
+      numCheck = false;
+      panel.innerText = ``;
+      inputFormula += char;
+    }
+  }
+}
+
+function reset() {
+  panel.innerText = `0`;
+}
+
+function calculate() {
+  if (inputFormula === ``) {
+  } else {
+    const result = eval(inputFormula);
+    panel.innerText = result;
+
+    let resultText;
+    resultText = inputFormula + ` = ` + result;
+
+    const resultDiv = document.createElement("div");
+    resultDiv.appendChild(document.createTextNode(resultText));
+
+    calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+
+    inputFormula = ``;
+  }
+}
