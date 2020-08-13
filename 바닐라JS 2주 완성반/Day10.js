304348c125237754f09381a525503d0bff54de79
LEEJEEWON <leejiwon6315@naver.com>
2020-08-13 21:54:28 +0900

    modified calculator function

--- 바닐라JS 2주 완성반/Day10.js
+++ 바닐라JS 2주 완성반/Day10.js
@@ -1,11 +1,25 @@
 const calcHistDiv = document.getElementById(`jsCalcHistory`);
 const panelForm = document.getElementById(`jsPanel`);
 const panel = panelForm.querySelector(`h3`);
-const regExp = document.getElementsByClassName(`regExpButton`);
+const num = document.querySelectorAll(`.jsNumber`);
+
+const regExp = document.querySelectorAll(`.regExpButton`);
+const resultBtn = document.getElementById(`resultButton`);
+const clearBtn = document.querySelector(`#acButton`);
 
 let inputFormula = ``;
 let numCheck = true;
 //연속된(잘못된) 계산 부호를 체크해주는 변수. 가장 먼저 입력했던 부호만 계산에 입력하기 위해 생성
+
+function callAdd(event) {
+  const charNum = event.target.innerText;
+  add(charNum);
+}
+
+function callFormula(event) {
+  const charExp = event.target.innerText;
+  formula(charExp);
+}
 
 function add(char) {
   numCheck = true;
@@ -41,16 +55,17 @@ function formula(char) {
 function reset() {
   panel.innerText = `0`;
   inputFormula = ``;
+  numCheck = true;
 }
 
 function calculate() {
   if (inputFormula === `` || numCheck === false) {
   } else {
+    console.log(inputFormula);
     const result = eval(inputFormula);
     panel.innerText = result;
 
-    let resultText;
-    resultText = inputFormula + ` = ` + result;
+    const resultText = inputFormula + ` = ` + result;
 
     const resultDiv = document.createElement("div");
     resultDiv.appendChild(document.createTextNode(resultText));
@@ -58,5 +73,16 @@ function calculate() {
     calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
 
     inputFormula = ``;
+    numCheck = true;
   }
 }
+
+for (let i = 0; i < 11; i++) {
+  num[i].addEventListener(`click`, callAdd);
+}
+for (let i = 0; i < 4; i++) {
+  regExp[i].addEventListener(`click`, callFormula);
+}
+
+resultBtn.addEventListener(`click`, calculate);
+clearBtn.addEventListener(`click`, reset);
